9fc46ff817f4d36fab17e649030683e8b1dc4d12
Constantinos Symeonides <csymeonides@gmail.com>
2017-09-13 17:10:56 +0100

    Change recursion to loop

--- src/nie/UnsignedNumberConverter.java
+++ src/nie/UnsignedNumberConverter.java
@@ -6,63 +6,83 @@ class UnsignedNumberConverter {
 	private String input;
 	private final StringBuilder output;
 	private String separator;
+	private int nextDigits;
 
 	UnsignedNumberConverter(String input, StringBuilder output) {
 		this.input = input;
 		this.output = output;
 		this.separator = "";
+		this.nextDigits = 0;
 	}
 
 	void convertUnsignedNumber() {
-		int numberOfDigits = input.length();
-		int numberOfTopDigits = ((numberOfDigits - 1) % 3) + 1;
-		int topDigits = Integer.valueOf(input.substring(0, numberOfTopDigits));
-		convertAtMostThreeDigits(topDigits);
-
-		if (numberOfDigits > 3) {
-			if (topDigits != 0) {
-				addPowerOfThousand(numberOfDigits);
+		while (hasMoreDigits()) {
+			setNextDigits();
+			if (nextDigits != 0) {
+				convertHundredsDigit();
+				convertRemainingDigits();
+				addPowerOfThousand();
 			}
-			convertRemainder(numberOfTopDigits);
+			setFinalSeparator();
 		}
 	}
 
-	private void convertAtMostThreeDigits(int digits) {
-		if (digits != 0) {
+	private boolean hasMoreDigits() {
+		return !input.isEmpty();
+	}
+
+	private void setNextDigits() {
+		int numberOfNextDigits = ((input.length() - 1) % 3) + 1;
+		nextDigits = Integer.valueOf(input.substring(0, numberOfNextDigits));
+		input = input.substring(numberOfNextDigits);
+	}
+
+	private void convertHundredsDigit() {
+		int hundredsDigit = nextDigits / 100;
+		if (hundredsDigit != 0) {
 			output.append(separator);
-			int hundredsDigit = digits / 100;
-			if (hundredsDigit > 0) {
-				output.append(SINGLE_DIGITS[hundredsDigit - 1]);
-				output.append(" hundred");
-				separator = " and ";
-				convertAtMostThreeDigits(digits % 100);
-			} else {
-				int tensDigit = digits / 10;
-				if (tensDigit > 1) {
-					output.append(TWENTY_TO_NINETY[tensDigit - 2]);
-					separator = " ";
-					convertAtMostThreeDigits(digits % 10);
-				} else if (tensDigit == 1) {
-					output.append(TEN_TO_NINETEEN[digits - 10]);
-				} else {
-					output.append(SINGLE_DIGITS[digits - 1]);
-				}
-			}
-		}
-	}
-
-	private void addPowerOfThousand(int numberOfDigits) {
-		int powersOfThousandIndex = ((numberOfDigits - 1) / 3) - 1;
-		output.append(" ");
-		output.append(POWERS_OF_THOUSAND[powersOfThousandIndex]);
-	}
-
-	private void convertRemainder(int offset) {
-		input = input.substring(offset);
-		separator = " ";
-		if (input.length() <= 3 && Integer.valueOf(input) < 100) {
+			output.append(SINGLE_DIGITS[hundredsDigit - 1]);
+			output.append(" hundred");
 			separator = " and ";
 		}
-		convertUnsignedNumber();
+	}
+
+	private void convertRemainingDigits() {
+		int tensDigit = (nextDigits % 100) / 10;
+		if (tensDigit == 1) {
+			output.append(separator);
+			output.append(TEN_TO_NINETEEN[(nextDigits % 100) - 10]);
+		} else {
+			if (tensDigit > 1) {
+				output.append(separator);
+				output.append(TWENTY_TO_NINETY[tensDigit - 2]);
+				separator = " ";
+			}
+			convertSingleDigit();
+		}
+	}
+
+	private void convertSingleDigit() {
+		int singleDigit = nextDigits % 10;
+		if (singleDigit != 0) {
+			output.append(separator);
+			output.append(SINGLE_DIGITS[singleDigit - 1]);
+		}
+	}
+
+	private void addPowerOfThousand() {
+		if (hasMoreDigits()) {
+			int powersOfThousandIndex = (input.length() - 1) / 3;
+			output.append(" ");
+			output.append(POWERS_OF_THOUSAND[powersOfThousandIndex]);
+		}
+	}
+
+	private void setFinalSeparator() {
+		if (hasMoreDigits() && input.length() <= 3 && Integer.valueOf(input) < 100) {
+			separator = " and ";
+		} else {
+			separator = " ";
+		}
 	}
 }
